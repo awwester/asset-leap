bf702a79f9b5da070f2b0a8831af4eff6fd8db05
Adam Wester <awwester@gmail.com>
2019-07-14 19:38:08 -0500

    Add statement date stuff.

--- frontend/src/components/dashboard/Header/index.js
+++ frontend/src/components/dashboard/Header/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default (props) => {
-  console.log('class namem...', props)
   return (
     <div className={`dashboard-header mb-4 ${props.className || ""}`}>
       {props.children}
--- frontend/src/routes/dashboard/Statements/index.js
+++ frontend/src/routes/dashboard/Statements/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Row, Col, Button, Container } from 'reactstrap';
+import { Row, Col, Button, Container, Input, Label } from 'reactstrap';
 import { toast } from 'react-toastify';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+import moment from 'moment';
 
+import LoadButton from 'components/buttons/LoadButton';
 import WorthTypeContainer from 'components/worth/TypeContainer'
 import DashboardHeader from 'components/dashboard/Header';
 import DashboardContainer from 'components/dashboard/Container';
+import './style.scss';
 
 class StatementsRoute extends React.Component {
   state = {
-    createMode: false
+    createMode: false,
+    statementDate: moment().startOf('month').format('YYYY-MM-DD')
   };
 
   createStatement = () => {
@@ -20,12 +24,16 @@ class StatementsRoute extends React.Component {
   }
 
   render() {
+    const dateIsValid = moment(this.state.statementDate, 'YYYY-MM-DD', true).isValid();
+    const formattedDate = moment(this.state.statementDate, "YYYY-MM-DD", true).format('LL')
+
     const renderCreateStatement = () => {
       // Return section for the user to create a new statement.
       const renderAssetGroups = () => {
         const uniqueAssetTypes = [...new Set(this.props.assets.map(obj => obj.type)) ];
         return uniqueAssetTypes.map(assetType => {
           return <WorthTypeContainer
+            key={assetType}
             worthItems={this.props.assets.filter(asset => asset.type === assetType)}
             type={assetType}
           />
@@ -36,6 +44,7 @@ class StatementsRoute extends React.Component {
         const uniqueLiabilityTypes = [...new Set(this.props.liabilities.map(obj => obj.type)) ];
         return uniqueLiabilityTypes.map(liabilityType => {
           return <WorthTypeContainer
+            key={liabilityType}
             worthItems={this.props.liabilities.filter(liab => liab.type === liabilityType)}
             type={liabilityType}
           />
@@ -44,18 +53,41 @@ class StatementsRoute extends React.Component {
 
       return (
         <div className="statements-section">
-          <h3 className="text-center m-5">Total worth: $300,000</h3>
+          <Row className="my-2">
+            <Col>
+              <Label className="mr-4">Statement Date</Label>
+              <Input
+                className="date-input"
+                placeholder="YYYY-MM-DD"
+                value={this.state.statementDate}
+                onChange={ev => this.setState({ statementDate: ev.target.value })}
+              />
+            </Col>
+          </Row>
           <Row>
             <Col>{renderAssetGroups()}</Col>
             <Col>{renderLiabilityGroups()}</Col>
           </Row>
           <div className="button-container text-right mt-4">
-            <Button color="link" onClick={() => this.setState({ createMode: false })}>
+            <Button
+              className="mr-4"
+              color="link"
+              onClick={() => this.setState({ createMode: false })}
+            >
               Cancel
             </Button>
-            <Button color="primary" onClick={this.createStatement}>
-              Create Statement
-            </Button>
+            <LoadButton
+              color="primary"
+              onClick={this.createStatement}
+              width={350}
+              isLoading={false}
+              disabled={!dateIsValid}
+            >
+              { dateIsValid ?
+               `Create Statement for ${formattedDate}` :
+               "Enter valid date (YYYY-MM-DD)"
+             }
+            </LoadButton>
           </div>
         </div>
       );
@@ -90,11 +122,13 @@ class StatementsRoute extends React.Component {
     }
 
     return (
-      <Container className="statements-route p-4">
-        {renderStatementsHeader()}
-        {renderStatementsContent()}
-      </Container>
-    )
+      <div className="statements-route">
+        <Container className="p-4">
+          {renderStatementsHeader()}
+          {renderStatementsContent()}
+        </Container>
+      </div>
+    );
   }
 }
 
